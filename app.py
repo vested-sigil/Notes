fb045d0ed4c177b0a0916c7c72ab48c7a7391960
vested-sigil <114979589+vested-sigil@users.noreply.github.com>
2024-03-11 22:46:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ from flask_cors import CORS
 from notion_client import Client
 import os
 import subprocess
+from dotenv import load_dotenv
 
-# Use the Environs module for better environment variable management
-from environs import Env
-env = Env()
-env.read_env()
+# Load environment variables
+load_dotenv()
+
+# Retrieve environment variables
+TOKEN = os.getenv('TOKEN')
+ROOT_UUID = os.getenv('ROOT_UUID')
+ACCOUNT_KEY = os.getenv('ACCOUNT_KEY')
+APPID = os.getenv('APPID')
 
 # Flask app initialization
 app = Flask(__name__)
@@ -16,33 +21,23 @@ CORS(app)
 api = Api(app)
 
 # Notion client initialization using environment variables
-NOTION_KEY = env.str("NOTION_KEY")
-NOTION_PAGE_ID = env.str("NOTION_PAGE_ID")
-client = Client(auth=NOTION_KEY)
+client = Client(auth=TOKEN)
 
 # Login route
 @app.route('/login', methods=['POST'])
 def login():
-    subprocess.run("./installer.sh")
-    account_key = os.getenv('ACCOUNT_KEY')
-    result = subprocess.run(["b4a", "configure", "accountkey", account_key], capture_output=True, text=True)
-    if result.returncode == 0:
-        return jsonify({"status": "success", "message": "Command executed successfully."}), 200
-    else:
-        return jsonify({"status": "error", "message": "Command failed with exit code", "error": result.stderr}), 500
+    subprocess.run("./installer.sh", check=True)
+    result = subprocess.run(["b4a", "configure", "accountkey", ACCOUNT_KEY], capture_output=True, text=True, check=True)
+    return jsonify({"status": "success", "message": "Command executed successfully."}), 200
 
 # Define your API resources here
 class RetrieveBlock(Resource):
     def get(self, block_id):
         return client.blocks.retrieve(block_id=block_id)
 
-# Add other resources similarly
-
 # Add resources to the API
 api.add_resource(RetrieveBlock, '/retrieve_block/<block_id>')
-# Add other resources similarly
 
 # Run the Flask app
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=6969)
-
